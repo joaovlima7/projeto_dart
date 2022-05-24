a771ec65a32ca5f28d07515b9bf5c62120d8cd7c
João Victor <joaovlima775@gmail.com>
2022-05-23 21:39:21 -0300

    first commit

---
+++ hello_world.dart
@@ -0,0 +1,10 @@
+//Curso de Tecnologia da Informação
+//Prof Sebastião Marcos
+
+
+//Função principal obrigatória
+void main() {
+  //Primeiro Programa em Dart
+  //Printando no terminal com Code Runner
+  print('Hello World!!!');
+}
